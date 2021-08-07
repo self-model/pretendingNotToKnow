215971114adc83ced28c2b6e3d0ffbcef83f48d9
Matan Mazor <matanmazor@outlook.com>
2021-08-07 17:23:00 +0100

    basic version - working

--- experiments/demos/exp1/jspsych-6.1.0/plugins/jspsych-battleships.js
+++ experiments/demos/exp1/jspsych-6.1.0/plugins/jspsych-battleships.js
@@ -9,8 +9,25 @@ jsPsych.plugins["Battleships"] = (function() {
 			grid: {
 				type: jsPsych.plugins.parameterType.INT,
 				pretty_name: "Grid",
-				default: [[0,0,0,0,0],[0,1,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0]],
+				default: [
+					['J','J','0','0','0','0','0','0','0','0'],
+					['0','0','0','F','F','F','0','0','C','0'],
+					['0','0','0','0','0','0','0','0','C','0'],
+					['B','B','B','B','0','0','I','0','C','0'],
+					['0','0','0','0','A','0','I','0','C','0'],
+					['G','G','0','0','A','0','0','0','0','0'],
+					['0','0','0','0','A','0','0','0','H','0'],
+					['0','D','0','0','A','0','0','0','H','0'],
+					['0','D','0','0','A','0','0','0','0','0'],
+					['0','D','0','0','0','0','0','E','E','E'],
+				],
 				description: "Grid"
+			},
+			cheat: {
+				type: jsPsych.plugins.parameterType.BOOL,
+				pretty_name:'cheat',
+				default: false,
+				description: 'Cheat'
 			}
 		}
 	}
@@ -25,24 +42,26 @@ jsPsych.plugins["Battleships"] = (function() {
 		const du = p.min([window.innerWidth, window.innerHeight], 600)*7/10 //drawing unit
 		const left_margin = p.round((window.innerWidth-du)/2);
 		const top_margin = p.round((window.innerHeight-du)/2);
-		console.log(top_margin)
 		const square_size = Math.floor(du/trial.grid.length);
-		console.log(square_size)
 		const colors = {
-			'sea':p.color(50,55,100),
-			'unknown':p.color(50),
-			'ship':p.color(100,50,55)
+			'sea':p.color(100,155,200),
+			'unknown':p.color(255,230,166),
+			'ship':p.color(200,50,55)
 		}
 		var grid_state = trial.grid.map(([...rest]) => rest.map(x => 'unknown'));
+		var click_log = {i:[],j:[],t:[],hit:[]};
+		const num_ones = trial.grid.flat().reduce((a,b)=>a+b,0);
+		var ships_hit = 0;;
+
 		function grid_coordinates_to_screen_coordinates(i,j) {
-			x=left_margin+i*square_size+Math.round(square_size/2);
-			y=top_margin+j*square_size+Math.round(square_size/2);
+			x=left_margin+j*square_size+Math.round(square_size/2);
+			y=top_margin+i*square_size+Math.round(square_size/2);
 			return({x:x,y:y})
 		}
 
 		function screen_coordinates_to_grid_coordinates(x,y) {
-			i = Math.floor((x-left_margin)/square_size);
-			j = Math.floor((y-top_margin)/square_size);
+			i = Math.floor((y-top_margin)/square_size);
+			j = Math.floor((x-left_margin)/square_size);
 			return({i:i,j:j})
 		}
 
@@ -50,71 +69,85 @@ jsPsych.plugins["Battleships"] = (function() {
 		p.setup = function() {
 
 			p.createCanvas(p.windowWidth, p.windowHeight);
-			p.background(128); //gray
 			p.strokeWeight(0);
 			p.frameRate(trial.frame_rate);
 			p.rectMode(p.CENTER)
 		}
 
-
-
 		//organize everything in one sequence
 		p.draw = function() {
-			// First, draw fixation cross
-			// if (p.millis()<10000) {
+
+			p.background(255);
+
+			// if (ships_hit<num_ones) {
+
 				for (var i=0; i<trial.grid.length; i++) {
 					for (var j=0; j<trial.grid.length; j++) {
 						xy = grid_coordinates_to_screen_coordinates(i,j);
-						window.grid_state = grid_state;
 						p.fill(colors[grid_state[i][j]]);
-						p.stroke(2);
-						p.strokeWeight(2)
+						p.stroke(127,182,177);
+						p.strokeWeight(1)
 						p.square(xy.x,xy.y,square_size);
 					}
 				}
 
+				if (trial.cheat) {
+					//mark ships with a cross
+					for (var i=0; i<trial.grid.length; i++) {
+						for (var j=0; j<trial.grid.length; j++) {
+							if (trial.grid[i][j]!='0') {
+								xy = grid_coordinates_to_screen_coordinates(i,j);
+								p.line(xy.x-square_size/2,xy.y-square_size/2,
+									xy.x+square_size/2,xy.y+square_size/2);
+								p.line(xy.x+square_size/2,xy.y-square_size/2,
+										xy.x-square_size/2,xy.y+square_size/2)
+							}
+						}
+					}
+				}
+
+				var text = `
+You clicked on ${click_log.t.length} squares.
+Overall you need to drown one 5-square carrier, two 4-square battleships,
+three 3-square submarines, and four 2-square patrol boats.`
+				p.push()
+				p.textAlign(p.Left, p.TOP)
+				p.textSize(15)
+				p.fill(0);
+				p.strokeWeight(0)
+				p.text(text,left_margin,top_margin-70)
+				p.pop()
 				// } else { //trial ended
 				// 	p.remove()
 				// 	// data saving
 				// 	var trial_data = {
-				// 		grid: trial.grid
+				// 		grid: trial.grid,
+				//    click_log: click_log
 				// 	};
-				//
+				// 	console.log(trial_data)
 				// 	// end trial
 				// 	jsPsych.finishTrial(trial_data);
 				// }
 			}
 
-			// p.keyPressed = function() {
-			// 	// it's only possible to query the key code once for each key press,
-			// 	// so saving it as a variable here:
-			// 	var key_code = p.keyCode
-			// 	// only regard relevant key presses during the response phase
-			// 	if (trial.status=='collecting response' &&
-			// 			trial.choices.includes(String.fromCharCode(key_code).toLowerCase())) {
-			// 		trial.response = String.fromCharCode(key_code).toLowerCase();
-			// 		trial.RT = p.millis()-trial.fixation_duration;
-			// 	}
-			// }
-			//
 			p.mouseClicked = function() {
 				ij = screen_coordinates_to_grid_coordinates(p.mouseX,p.mouseY)
 				// make sure click is on the board
 				if (ij.i>=0 & ij.i<trial.grid.length & ij.j>=0 & ij.j<trial.grid.length) {
-					if (trial.grid[i][j]==0) {
-						grid_state[i][j]='sea'
-					} else if (trial.grid[i][j]==1) {
-						grid_state[i][j]='ship'
+					if (trial.grid[ij.i][ij.j]=='0') {
+						grid_state[ij.i][ij.j]='sea'
+					} else {
+						grid_state[ij.i][ij.j]='ship';
+						ships_hit+=1
 					}
+
+					click_log.i.push(ij.i);
+					click_log.j.push(ij.j);
+					click_log.t.push(p.millis())
+					click_log.hit.push((trial.grid[ij.i][ij.j]))
 				}
 			}
-			//
-			// // only present confidence circle after the mouse has moved
-			// p.mouseMoved = function() {
-			// 	if (trial.status=='collecting confidence') {
-			// 		trial.present_circle = 1
-			// 	}
-			// }
+
 		};
 
 		// start sketch!
